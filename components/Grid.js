8f4fd7b97817902495fab466e36e9ad15beff398
Sahar Atat <saharatat325@gmail.com>
2026-03-26 10:44:14 +0200

    hover on grid

--- components/Grid.js
+++ components/Grid.js
@@ -10,13 +10,13 @@ export default function Grid() {
     return (
 
       <div className="container mx-auto px-4 text-[palevioletred]">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 overflow-hidden">
  
       {imgTxt.map((item, index) => (
         <div className="grid-item" key={index}>
           <img src={item.src} alt={item.text}
-            className="w-full h-125 object-cover" />
-          <p className="color-pa text-center mt-2">{item.text}</p>
+            className="w-full h-125 object-cover scale-95 hover:scale-110" />
+          <p className="color-pa text-center mt-2 hover:underline hover:cursor-pointer">{item.text}</p>
         </div>
       ))}
         

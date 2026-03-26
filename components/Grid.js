0e445574e6efcd7f1ddf58cb2d7dda00ece0a230
Sahar Atat <saharatat325@gmail.com>
2026-03-26 10:58:46 +0200

    smooth transition

--- components/Grid.js
+++ components/Grid.js
@@ -10,14 +10,17 @@ export default function Grid() {
     return (
 
       <div className="container mx-auto px-4 text-[palevioletred]">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 overflow-hidden">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
  
       {imgTxt.map((item, index) => (
         <div className="grid-item" key={index}>
+          <div className="overflow-hidden">
           <img src={item.src} alt={item.text}
-            className="w-full h-125 object-cover scale-95 hover:scale-110" />
+            className="w-full h-125 object-cover scale-95 hover:scale-110 ease-in-out transition-transform duration-500" />
+            </div>
           <p className="color-pa text-center mt-2 hover:underline hover:cursor-pointer">{item.text}</p>
         </div>
+
       ))}
         
       </div>

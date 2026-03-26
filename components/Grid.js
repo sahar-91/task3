3750de6aa6a91bae977381491e565a901813b5eb
Sahar Atat <saharatat325@gmail.com>
2026-03-26 11:22:56 +0200

    update

--- components/Grid.js
+++ components/Grid.js
@@ -13,12 +13,12 @@ export default function Grid() {
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
  
       {imgTxt.map((item, index) => (
-        <div className="grid-item" key={index}>
+        <div className="grid-item group" key={index}>
           <div className="overflow-hidden">
           <img src={item.src} alt={item.text}
-            className="w-full h-125 object-cover scale-95 hover:scale-110 ease-in-out transition-transform duration-500" />
+            className="w-full h-125 object-cover group-hover:scale-110 ease-in-out transition-transform duration-500" />
             </div>
-          <p className="color-pa text-center mt-2 hover:underline hover:cursor-pointer">{item.text}</p>
+          <p className="color-pa text-center mt-2 group-hover:underline hover:cursor-pointer">{item.text}</p>
         </div>
 
       ))}

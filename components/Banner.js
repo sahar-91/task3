872808c06ea1401ec0d229009c8f861948bb1865
Sahar Atat <saharatat325@gmail.com>
2026-03-26 15:23:52 +0200

    adding/modifying components

--- components/Banner.js
+++ components/Banner.js
@@ -1,12 +1,13 @@
-export default function Banner() {
-    return (
-    <div className="bg-[url('/cat-banner.jpeg')] bg-cover p-20 text-white flex  items-center">
+export default function Banner({ title, description, bgImage }) {
+  return (
+    <div
+      className={`bg-cover p-20 text-white flex items-center`}
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <div className="container mx-auto px-4">
-        <h1>Welcome to Our Website</h1>
-        <p className="max-w-100 w-full">
-          A place where you can find cute kitties and learn how to take proper care of them. Discover what foods are healthy for cats, such as high-protein meals and safe treats, and what to avoid like chocolate, onions, or overly salty foods. Learn about daily care, grooming, and how to keep your cat active and happy. You can also explore adoption options, understand cat behavior, and get tips to create a safe and comfortable environment for your furry friend.
-        </p>
+        <h1 className="text-4xl font-bold mb-4">{title}</h1>
+        <p className="max-w-100 w-full text-lg">{description}</p>
       </div>
     </div>
-    );
+  );
 }

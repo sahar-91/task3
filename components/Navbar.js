8a86544c095b2be78558dce426a66fc3b8fb408f
Sahar Atat <saharatat325@gmail.com>
2026-03-31 10:37:09 +0300

    underline link of the page when the it is open

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,16 @@
 "use client";  
 import { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const [active, setActive] = useState(false);
+  const pathname = usePathname();
+
+  const linkUndeline = (path) =>
+    `hover:text-pink-600 ${
+      pathname === path ? "underline text-[palevioletred]" : ""
+    }`;
 
   return (
       <div className="bg-white sticky top-0 z-100">
@@ -32,11 +39,11 @@ export default function Navbar() {
             x
           </div>
           <ul className="flex gap-2.5 list-none md:flex-row flex-col">
-            <li className="hover:text-pink-600"><Link href="/">Home</Link></li>
-            <li className="hover:text-pink-600"><Link href="/adopt">Adopt</Link></li>
-            <li className="hover:text-pink-600"><Link href="/catCare">Cat care</Link></li>
-            <li className="hover:text-pink-600"><Link href="/contact">Contact</Link></li>
-          </ul>
+            <li className={`hover:text-pink-600 ${linkUndeline("/")}`}><Link href="/">Home</Link></li>
+            <li className={`hover:text-pink-600 ${linkUndeline("/adopt")}`}><Link href="/adopt">Adopt</Link></li>
+            <li className={`hover:text-pink-600 ${linkUndeline("/catCare")}`}><Link href="/catCare">Cat care</Link></li>
+            <li className={`hover:text-pink-600 ${linkUndeline("/contact")}`}><Link href="/contact">Contact</Link></li>
+          </ul>e"
         </nav>
       </header>
     </div>

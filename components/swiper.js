1a5dc8f1f92a98a0fba075c67500eba545f0e87d
Sahar Atat <saharatat325@gmail.com>
2026-04-01 14:13:29 +0300

    pagination and navigation

--- components/swiper.js
+++ components/swiper.js
@@ -1,44 +1,49 @@
 'use client';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import "swiper/css/pagination"; 
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import { useEffect } from 'react';
 
 
 export default function SwiperComp({swipe}) {
   useEffect(() => {
     new Swiper(".swiper-container", {
-      modules: [Navigation], 
+      modules: [Navigation, Pagination], 
       spaceBetween: 16,      
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      }, 
       keyboard: { enabled: true },
       grabCursor: true,    
       slidesPerView: 1,
       breakpoints: {
         640: { slidesPerView: 2 },
         768: { slidesPerView: 3 },
-      }
+      },
+      navigation: true,
+      pagination: { 
+        el: '.swiper-pagination',
+        clickable: true },
 
     });
   }, []);
 
   return (
-    <div className="swiper-container overflow-x-auto whitespace-nowrap py-4">
-      <div className="swiper-wrapper flex space-x-4">
+    <div className="max-w-[1440px] overflow-hidden mx-auto">
+    <div className="swiper-container py-4">
+      
+      <div className="swiper-wrapper">
         {swipe.map((img, index) => (
-          <div key={index} className="swiper-slide inline-block">
+          <div key={index} className="swiper-slide ">
             <img
               src={img.image}  
               alt={img.title} 
-              className="w-full h-100 rounded-lg"
+              className="w-full  h-64 object-cover rounded-lg"
             />
           </div>
         ))}
       </div>
+      <div className="swiper-pagination"></div>
+    </div>
     </div>
   );
 }
